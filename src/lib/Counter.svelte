b8c4414275d3136e7bba67ad4c14c441688db1c1
lukasWastaken <l.leutgeb@students.htl-leonding.ac.at>
2025-03-05 17:50:53 +0100

    Updated some UI stuff

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,10 +1,12 @@
 <script>
-  let count = $state(0)
+  let count = $state(parseInt(localStorage.getItem('count')) || 0);
+
   const increment = () => {
-    count += 1
+    count += 1;
+    localStorage.setItem('count', count);
   }
 </script>
 
-<button onclick={increment}>
+<button class="button" onclick={increment}>
   count is {count}
 </button>
